837cf584ade6cad977c6bb5502be6d5416c5a47b
Sorawit <thankzaki@hotmail.com>
2025-07-26 15:08:31 +0700

    Step 4: Dynamic Variable Type

---
+++ dart4.dart
@@ -0,0 +1,11 @@
+// Dynamic Variable Type
+void main() {
+  dynamic dynamicVariable = 42; // Initially an integer
+  print(dynamicVariable); // Output: 42
+
+  dynamicVariable = "Hello, Dart!"; // Now a string
+  print(dynamicVariable); // Output: Hello, Dart!
+
+  dynamicVariable = [1, 2, 3]; // Now a list
+  print(dynamicVariable); // Output: [1, 2, 3]
+}
