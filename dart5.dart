e6dfe1f05def6d309b8dec1009e601895ff80eb4
Sorawit <thankzaki@hotmail.com>
2025-07-26 15:18:43 +0700

    Step 5: Arithmetic operations in Dart

---
+++ dart5.dart
@@ -0,0 +1,35 @@
+// Arithmetic operations in Dart
+
+void main() {
+  int a = 10;
+  int b = 5;
+
+  // Addition
+  int sum = a + b;
+  print("Sum: $sum"); // Output: Sum: 15
+
+  // Subtraction
+  int difference = a - b;
+  print("Difference: $difference"); // Output: Difference: 5
+
+  // Multiplication
+  int product = a * b;
+  print("Product: $product"); // Output: Product: 50
+
+  // Division
+  double quotient = a / b;
+  print("Quotient: $quotient"); // Output: Quotient: 2.0
+
+  // Modulus
+  int remainder = a % b;
+  print("Remainder: $remainder"); // Output: Remainder: 0
+
+  print("-------");
+
+  int m = 10;
+
+  print("++m = ${++m}");
+  print("m++ = ${m++}");
+  print("--m = ${--m}");
+  print("m-- = ${m--}");
+}
