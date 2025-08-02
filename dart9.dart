6233bbc75f90798fcc1dfe66f23bc24215f23a0a
Sorawit <thankzaki@hotmail.com>
2025-08-02 10:32:11 +0700

    Step 9: Function

---
+++ dart9.dart
@@ -0,0 +1,19 @@
+// Function
+
+void main() {
+  printMsg("Joel");
+  print(getMsg("Joel"));
+  print(add(10, 20));
+}
+
+void printMsg(String name) {
+  print("Hello1, $name");
+}
+
+String getMsg(String name) {
+  return "Hello2, $name";
+}
+
+int add(int a, int b) {
+  return a + b;
+}
